db181f9434236963ccac9a7938803b59b3996f01
Najam us Saqib <najamforit@gmail.com>
2024-07-09 15:15:55 +0500

    cc..

---
+++ src/app/audios/page.js
@@ -0,0 +1,49 @@
+import Head from "next/head";
+export default function Audios() {
+  return (
+    <>
+     <Head>
+        <title>Audio Library - Lead U</title>
+        <meta
+          name="description"
+          content="Explore our collection of educational audios at Lead U. Listen to enriching content and expand your knowledge with our audio resources."
+        />
+        <meta
+          name="keywords"
+          content="Lead U Audio Library, Educational Audios, ISM-E-AZAM, Learning Resources, Audio Files, Listen and Learn"
+        />
+        <meta name="author" content="Lead U" />
+        <meta property="og:title" content="Audio Library - Lead U" />
+        <meta
+          property="og:description"
+          content="Discover our audio library featuring a wide range of educational content. Enhance your learning experience with our audio resources at Lead U."
+        />
+        <meta property="og:image" content="/leadu-logo.png" />
+        <meta property="og:url" content="https://www.leadu.pk/audios" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Audio Library - Lead U" />
+        <meta
+          name="twitter:description"
+          content="Access a variety of educational audios at Lead U. From ISM-E-AZAM to other insightful content, our audio library has something for everyone."
+        />
+        <meta name="twitter:image" content="/leadu-logo.png" />
+        <link rel="canonical" href="https://www.leadu.pk/audios" />
+      </Head>
+      <div className="justify-center bg-white">
+        <div className="col mx-auto  py-10 md:w-1/2 sm:w-full lg:w-1/2">
+          <div className="my-4 text-center flex flex-col gap-2">
+            <h3 className="font-bold text-xl my-2">ISM-E-AZAM</h3>
+            <p className="my-2">Click the play button.</p>
+            <audio controls preload="none" className="my-2 w-full max-w-md mx-auto">
+              <source
+                src="https://leadu.pk/wp-content/uploads/2019/12/ISM-E-AZAM.mp3"
+                type="audio/mpeg"
+              />
+              Your browser does not support the audio element.
+            </audio>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/app/takhasus-fil-quran/page.js
@@ -0,0 +1,155 @@
+import Image from "next/image";
+import Head from "next/head";
+export default function About_Us() {
+  return (
+    <>
+       <Head>
+        <title>Takhasus-fil-Quran - Lead U</title>
+        <meta
+          name="description"
+          content="Explore our specialized Takhasus-fil-Quran program designed to deepen your understanding and recitation of the Quran. Join Lead U to advance your Quranic studies."
+        />
+        <meta
+          name="keywords"
+          content="Takhasus-fil-Quran, Quranic Studies, Quran Recitation, Lead U, Advanced Quran Program"
+        />
+        <meta name="author" content="Lead U" />
+        
+        <meta property="og:title" content="Takhasus-fil-Quran - Lead U" />
+        <meta
+          property="og:description"
+          content="Join the Takhasus-fil-Quran program at Lead U to enhance your knowledge and proficiency in Quranic studies. Our curriculum is designed for advanced learners seeking deeper insights into the Quran."
+        />
+        <meta property="og:image" content="/leadu-logo.png" />
+        <meta property="og:url" content="https://www.leadu.pk/takhasus-fil-quran" />
+        
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Takhasus-fil-Quran - Lead U" />
+        <meta
+          name="twitter:description"
+          content="Enroll in the Takhasus-fil-Quran program at Lead U to advance your Quranic studies. Our expert-led courses are designed to provide deep and comprehensive knowledge of the Quran."
+        />
+        <meta name="twitter:image" content="/leadu-logo.png" />
+        
+        <link rel="canonical" href="https://www.leadu.pk/takhasus-fil-quran" />
+      </Head>
+      <main className="flex flex-col items-center justify-between">
+        <div className="relative w-full md:h-[491px] justify-center items-center">
+          <Image
+            src="/banner.jpg"
+            alt="Lead U Background"
+            width={1500}
+            height={382}
+            style={{ objectFit: 'cover' }}
+            className="cursor-pointer w-full md:h-[100%]"
+            title="Lead U Background"
+          />        
+        </div>
+      </main>
+      <section className="pb-10 pt-2 bg-white">
+        <div className="relative  py-10 flex justify-center items-center">
+          <div className="text-center max-w-4xl mx-auto">
+          <h1 className="md:text-2xl text-xl font-noraml tracking-wide uppercase">Syllabus/Properties</h1>
+            <div className="hover:opacity-90 flex justify-center w-full">
+              <Image
+                src="/sallybus-1536x530.jpg"
+                alt="Welcome Note"
+                width={436}
+                height={156}
+                className="md:w-auto w-[70%]"
+              />
+            </div>
+            <div className="my-6 pb-5">
+              <div className="hover:opacity-90">
+                <Image
+                  src="/tajweed-1536x543.jpg"
+                  alt="tajweed"
+                  width={874}
+                  height={997}
+                  className="hover:grayscale h-full w-full border border-neutral-200 hover:shadow-lg rounded-lg"
+                />
+              </div>
+            </div>
+            <div className="my-6 pb-5">
+              <div className="hover:opacity-90">
+                <Image
+                  src="/arabic-1536x780.jpg"
+                  alt="arabic"
+                  width={874}
+                  height={997}
+                  className="hover:grayscale h-full w-full border border-neutral-200 hover:shadow-lg rounded-lg"
+                />
+              </div>
+            </div>
+            <div className="my-6 pb-5">
+              <div className="hover:opacity-90">
+                <Image
+                  src="/uloom-ul-quran-1536x1442.jpg"
+                  alt="uloom-ul-quran"
+                  width={874}
+                  height={997}
+                  className="hover:grayscale h-full w-full border border-neutral-200 hover:shadow-lg rounded-lg"
+                />
+              </div>
+            </div>
+            <div className="my-6 pb-5">
+              <div className="hover:opacity-90">
+                <Image
+                  src="/dawat-1536x941.jpg"
+                  alt="dawat"
+                  width={874}
+                  height={997}
+                  className="hover:grayscale h-full w-full border border-neutral-200 hover:shadow-lg rounded-lg"
+                />
+              </div>
+            </div> 
+            <div className="my-6 pb-5">
+              <div className="hover:opacity-90">
+                <Image
+                  src="/computer-768x1085.jpg"
+                  alt="Computer"
+                  width={874}
+                  height={997}
+                  className="hover:grayscale h-full w-full border border-neutral-200 hover:shadow-lg rounded-lg"
+                />
+              </div>
+            </div> 
+            <div className="my-6 pb-5">
+              <div className="hover:opacity-90">
+                <Image
+                  src="/geography-768x1085.jpg"
+                  alt="geography"
+                  width={874}
+                  height={997}
+                  className="hover:grayscale h-full w-full border border-neutral-200 hover:shadow-lg rounded-lg"
+                />
+              </div>
+            </div> 
+            <div className="my-6 pb-5">
+              <div className="hover:opacity-90">
+                <Image
+                  src="/astronomy-768x1085.jpg"
+                  alt="astronomy"
+                  width={874}
+                  height={997}
+                  className="hover:grayscale h-full w-full border border-neutral-200 hover:shadow-lg rounded-lg"
+                />
+              </div>
+            </div> 
+            <div className="my-6 pb-5">
+              <div className="hover:opacity-90">
+                <Image
+                  src="/tarbiyat-1183x1536.jpg"
+                  alt="tarbiyat"
+                  width={874}
+                  height={997}
+                  className="hover:grayscale h-full w-full border border-neutral-200 hover:shadow-lg rounded-lg"
+                />
+              </div>
+            </div>       
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
